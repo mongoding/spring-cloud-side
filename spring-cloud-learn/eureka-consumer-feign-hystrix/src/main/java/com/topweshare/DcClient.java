e625f315d76bf79d2d5b40d5e3de9fb754ea7e07
dingzhenhao <dingzhenhao@jiuxian.com>
2018-05-03 13:08:54 +0800

    提供者和消费者

--- spring-cloud-learn/eureka-consumer-feign-hystrix/src/main/java/com/topweshare/DcClient.java
+++ spring-cloud-learn/eureka-consumer-feign-hystrix/src/main/java/com/topweshare/DcClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @create 2017/6/24.
  * @blog https://mongoding.github.io
  */
-@FeignClient(name = "eureka-feign-client-provider", fallback = DcClientFallback.class)
+@FeignClient(name = "user-service-provider", fallback = DcClientFallback.class)
 public interface DcClient extends HelloService{
 
     @GetMapping("/dc")
